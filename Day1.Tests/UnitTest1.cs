6a832274f7285dfdebb4e7b99f9870c40d6b1c12
Cail Young <cail.young@octopus.com>
2022-02-24 16:17:12 +1100

    add windowing test

--- Day1.Tests/UnitTest1.cs
+++ Day1.Tests/UnitTest1.cs
@@ -1,11 +1,64 @@
+using System;
+using System.Linq;
 using Xunit;
+using Advent2021;
 
 namespace Day1.Tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void ExampleReturnsCorrectOutput()
     {
+        var exampleInput = @"
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+".Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Assert.Equal(7, Calculators.CountMeasurementIncreases(exampleInput));
+    }
+
+    [Fact]
+    public void WindowedSumsCalculateCorrectly()
+    {
+        var exampleInput = @"
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+".Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        var expectedOutput = @"
+607
+618
+618
+617
+647
+716
+769
+792
+".Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        Assert.Equal(expectedOutput, Calculators.ProduceWindowedSums(exampleInput, 3));
     }
 }

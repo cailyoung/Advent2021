625dd2746e0ac3cf044e1184a994f7fb0b089efa
Cail Young <cail.young@octopus.com>
2022-02-28 17:25:00 +1100

    remove unused comment

--- Day3.Tests/UnitTest1.cs
+++ Day3.Tests/UnitTest1.cs
@@ -73,22 +73,6 @@ public class UnitTest1
         Assert.Equal(expectedOutput, actualOutput);
     }
 
-
-/*
-00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010
-*/
-
     [Fact]
     public void OxygenRateCalculatorIsCorrect()
     {

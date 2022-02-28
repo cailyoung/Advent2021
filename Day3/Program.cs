5471667bcfe9315dc1f4bd508fe4ad947fe1e2b0
Cail Young <cail.young@octopus.com>
2022-02-28 17:16:23 +1100

    part 1 solution

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Part 1
+
+using Day3;
+
+var input = FileHelper.ExtractInputFromFile("day3input.txt");
+
+var parsedInput = FileHelper.ParseInput(input).ToList();
+
+var part1Gamma = Calculators.CalculateGammaRate(parsedInput);
+var part1Epsilon = Calculators.CalculateEpsilonRate(parsedInput);
+
+var part1PowerConsumption = part1Gamma * part1Epsilon;
+
+Console.WriteLine($"Part 1 power consumption is {part1PowerConsumption}");

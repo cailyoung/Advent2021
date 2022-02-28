dba2d2da7cc545ece2b17ca20a4809c389c4863e
Cail Young <cail.young@octopus.com>
2022-02-28 13:29:31 +1100

    working parser

--- Day3.Tests/UnitTest1.cs
+++ Day3.Tests/UnitTest1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FluentAssertions;
 using Xunit;
 
 namespace Day3.Tests;
@@ -5,7 +9,23 @@ namespace Day3.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void ParserReturnsValidOutput()
     {
+        var testInput = @"
+00100
+11110
+10110
+".Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        var expectedOutput = new List<BitArray>
+        {
+            new(new[] {false, false, true, false, false}),
+            new(new[] {true, true, true, true, false}),
+            new(new[] {true, false, true, true, false})
+            };
+        
+        var output = FileHelper.ParseInput(testInput);
+
+        output.Should().BeEquivalentTo(expectedOutput);
     }
 }
--- Day3/FileHelper.cs
+++ Day3/FileHelper.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using System.Globalization;
+
 namespace Day3;
 
 public class FileHelper
@@ -7,5 +10,23 @@ public class FileHelper
         var rawInput = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, inputFileName));
 
         return rawInput;
+    }
+
+    public static IEnumerable<BitArray> ParseInput(string[] input)
+    {
+        var response = new List<BitArray>();
+        
+        foreach (var row in input)
+        {
+            var foo = row.Select(s => s.ToString()).ToArray();
+
+            var fooAsInts = foo.Select(s => Convert.ToInt32(s)).ToArray();
+
+            var bitArray = new BitArray(fooAsInts.Select(Convert.ToBoolean).ToArray());
+            
+            response.Add(bitArray);
+        }
+
+        return response;
     }
 }

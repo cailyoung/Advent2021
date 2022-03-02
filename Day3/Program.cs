c8a494330cc115f044d62d7dd104bae94c0f958c
Cail Young <cail.young@octopus.com>
2022-03-02 14:27:18 +1100

    part 2 solution

--- Day3/Program.cs
+++ Day3/Program.cs
@@ -14,3 +14,12 @@ var part1Epsilon = Calculators.CalculateEpsilonRate(parsedInput);
 var part1PowerConsumption = part1Gamma * part1Epsilon;
 
 Console.WriteLine($"Part 1 power consumption is {part1PowerConsumption}");
+
+// Part 2
+
+var part2Oxygen = Calculators.CalculateOxygenRate(parsedInput);
+var part2Co2 = Calculators.CalculateCarbonDioxideRate(parsedInput);
+
+var part2LifeSupportRating = part2Oxygen * part2Co2;
+
+Console.WriteLine($"Part 2 life support rating is {part2LifeSupportRating}");

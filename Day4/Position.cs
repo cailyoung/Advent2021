5cf104c7a1748ba5b7839bf52e66ce81e03bf417
Cail Young <cail.young@octopus.com>
2022-03-10 15:03:16 +1100

    extract Position.cs

---
+++ Day4/Position.cs
@@ -0,0 +1,26 @@
+namespace Day4;
+
+public class Position
+{
+    private int Value { get; }
+    public bool Called { get; set; }
+    internal int CoordinateX { get; }
+    internal int CoordinateY { get; }
+
+    public Position(int coordinateX, int coordinateY, int value, bool called = false)
+    {
+        Value = value;
+        Called = called;
+        CoordinateX = coordinateX;
+        CoordinateY = coordinateY;
+    }
+
+    public static IEnumerable<Position> GeneratePositionRow(IEnumerable<int> values, int rowNumber, bool called = false)
+    {
+        var valueArray = values.ToArray();
+        
+        return Enumerable
+            .Range(0, valueArray.Length)
+            .Select((_, i) => new Position(i, rowNumber, valueArray[i], called));
+    }
+}

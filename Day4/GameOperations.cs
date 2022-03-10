5427e29d7bbff4114872df6de83ed27b4db02f4b
Cail Young <cail.young@octopus.com>
2022-03-10 15:27:31 +1100

    scaffolding

---
+++ Day4/GameOperations.cs
@@ -0,0 +1,25 @@
+using System.Collections.Immutable;
+
+namespace Day4;
+
+public class GameOperations
+{
+    public GameStep CallNextBingoNumber(GameStep previousStep)
+    {
+        if (previousStep.BingoNumbersToCall.Count == 0)
+        {
+            throw new ArgumentException("There are no numbers left to call.");
+        }
+
+        var numberToCall = previousStep.BingoNumbersToCall.First();
+
+        return new GameStep(
+            CallNumberOnBoards(numberToCall, previousStep.Boards),
+            previousStep.BingoNumbersToCall.Skip(1).ToImmutableList());
+    }
+
+    private List<BingoBoard> CallNumberOnBoards(int numberToCall, List<BingoBoard> boards)
+    {
+        return boards;
+    }
+}
--- Day4/GameStep.cs
+++ Day4/GameStep.cs
@@ -1,8 +1,16 @@
+using System.Collections.Immutable;
+
 namespace Day4;
 
 public class GameStep
 {
-    public List<BingoBoard> Boards;
+    public List<BingoBoard> Boards { get; }
 
-    public List<int> BingoNumbersToCall;
+    public ImmutableList<int> BingoNumbersToCall { get; }
+
+    public GameStep(List<BingoBoard> boards, ImmutableList<int> bingoNumbersToCall)
+    {
+        Boards = boards;
+        BingoNumbersToCall = bingoNumbersToCall;
+    }
 }

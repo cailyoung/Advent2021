77a7a584074d5758c8460a169702b0ea1064b5a9
Cail Young <cail.young@octopus.com>
2022-03-11 22:18:45 +1100

    failing test for part 2

--- Day4.Tests/UnitTest1.cs
+++ Day4.Tests/UnitTest1.cs
@@ -346,4 +346,70 @@ public class UnitTest1
         Assert.False(uncalledPositionViolations);
 
     }
+    
+        [Fact]
+    public void RunningGameUntilFinalWinProducesExpectedLastNumber()
+    {
+                var positionListOne = new List<Position>();
+        positionListOne.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 22, 13, 17, 11, 0 }, 0));
+        positionListOne.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 8, 2, 23, 4, 24 }, 1));
+        positionListOne.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 21, 9, 14, 16, 7 }, 2));
+        positionListOne.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 6, 10, 3, 18, 5 }, 3));
+        positionListOne.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 1, 12, 20, 15, 19 }, 4));
+        
+        var boardOne = new BingoBoard(positionListOne);
+        
+        var positionListTwo = new List<Position>();
+        positionListTwo.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 3, 15, 0, 2, 22 }, 0));
+        positionListTwo.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 9, 18, 13, 17, 5 }, 1));
+        positionListTwo.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 19, 8, 7, 25, 23 }, 2));
+        positionListTwo.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 20, 11, 10, 24, 4 }, 3));
+        positionListTwo.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 14, 21, 16, 12, 6 }, 4));
+        
+        var boardTwo = new BingoBoard(positionListTwo);
+        
+        var positionListThree = new List<Position>();
+        positionListThree.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 14, 21, 17, 24, 4 }, 0));
+        positionListThree.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 10, 16, 15, 9, 19 }, 1));
+        positionListThree.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 18, 8, 23, 26, 20 }, 2));
+        positionListThree.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 22, 11, 13, 6, 5 }, 3));
+        positionListThree.AddRange(Position
+            .GeneratePositionRow(new List<int>() { 2, 0, 12, 3, 7 }, 4));
+        
+        var boardThree = new BingoBoard(positionListThree);
+
+        var initialBoardList = ImmutableList.Create(boardOne, boardTwo, boardThree);
+        var initialNumbersToCall = new[]
+        {
+            7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1
+        };
+        var initialStep = new GameStep(initialBoardList, initialNumbersToCall.ToImmutableList());
+
+        var finalState = GameOperations.RunGameUntilFinalWin(initialStep);
+        var finalCalledNumber = finalState.LastCalledNumber;
+        
+        Assert.Equal(13, finalCalledNumber);
+
+        var winningBoard = GameOperations.GetWinningBoards(finalState).Single();
+
+        var totalUncalledValuesFromWinningBoard = winningBoard.TotalUncalledValues;
+        
+        Assert.Equal(148, totalUncalledValuesFromWinningBoard);
+        
+        Assert.Equal(1924, totalUncalledValuesFromWinningBoard * finalCalledNumber);
+    }
 }

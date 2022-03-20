e77271c093a764c502c2002beeb34bc0b484e47b
Cail Young <cail.young@octopus.com>
2022-03-20 21:31:47 +1100

    scaffold

---
+++ Day5/Calculators.cs
@@ -0,0 +1,11 @@
+using System.Collections.Immutable;
+
+namespace Day5;
+
+public class Calculators
+{
+    public static ImmutableList<(CoOrd, int)> Overlaps(MapGrid currentGrid)
+    {
+        return ImmutableList<(CoOrd, int)>.Empty;
+    }
+}

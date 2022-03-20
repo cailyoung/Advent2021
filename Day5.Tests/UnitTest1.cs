aa378fa6b9a976ad1d8e0f51bcdec466aae18d2f
Cail Young <cail.young@octopus.com>
2022-03-20 21:34:13 +1100

    failing test

--- Day5.Tests/UnitTest1.cs
+++ Day5.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
 using FluentAssertions;
 using Xunit;
 
@@ -87,5 +88,33 @@ public class UnitTest1
         var exception = Record.Exception(() => new MapGrid(inputVentLines));
         
         Assert.Null(exception);
+    }
+
+    [Fact]
+    public void MapGridReturnsRightPart1Value()
+    {
+        var rawInput = @"
+0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2
+".Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+        var filteredInput = FileHelper
+            .ExtractVentLinesFromFile(rawInput)
+            .Where(line => !line.Diagonal)
+            .ToImmutableList();
+
+        var result = Calculators
+            .Overlaps(new MapGrid(filteredInput))
+            .Count(v => v.overlapCount >= 2);
+        
+        Assert.Equal(5, result);
     }
 }

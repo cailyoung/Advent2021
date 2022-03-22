6c4295dbe3152a59887a00c6039201eb53389b05
Cail Young <cail.young@octopus.com>
2022-03-22 17:17:42 +1100

    part 2 :tada:

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -16,4 +16,17 @@ var overlaps = Calculators.Overlaps(mapGrid);
 
 var atLeastTwoOverlapsCount = overlaps.Count(p => p.overlapCount >= 2);
 
-Console.WriteLine($"For the full input, the number of points where at least two lines overlap is {atLeastTwoOverlapsCount}");
+Console.WriteLine($"For the full input without diagonals, the number of points where at least two lines overlap is {atLeastTwoOverlapsCount}");
+
+// Part 2
+
+var part2ParsedInput = FileHelper
+    .ExtractVentLinesFromFile(rawInput);
+
+var part2MapGrid = new MapGrid(part2ParsedInput);
+
+var part2Overlaps = Calculators.Overlaps(part2MapGrid);
+
+var part2AtLeast2OverlapsCount = part2Overlaps.Count(p => p.overlapCount >= 2);
+
+Console.WriteLine($"For the full input, the number of points where at least two lines overlap is {part2AtLeast2OverlapsCount}");

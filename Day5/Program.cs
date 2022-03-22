51dde3d494d892eef9cb9f01e73e754ae6394b40
Cail Young <cail.young@octopus.com>
2022-03-22 14:30:35 +1100

    part 1

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -1,3 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Part 1
+
+using Day5;
+
+var rawInput = FileHelper.ExtractInputFromFile("day5input.txt");
+
+var parsedInput = FileHelper
+    .ExtractVentLinesFromFile(rawInput)
+    .Where(line => !line.Diagonal);
+
+var mapGrid = new MapGrid(parsedInput);
+
+var overlaps = Calculators.Overlaps(mapGrid);
+
+var atLeastTwoOverlapsCount = overlaps.Count(p => p.overlapCount >= 2);
+
+Console.WriteLine($"For the full input, the number of points where at least two lines overlap is {atLeastTwoOverlapsCount}");

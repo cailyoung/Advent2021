7a04747deb38f68207f1ee83a43fc101ca08e2e9
Cail Young <cail.young@octopus.com>
2022-03-23 21:32:23 +1100

    part 1

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+// Part 1
+
+using Day6;
+
+var input = FileHelper.ExtractInputFromFile("day6input.txt");
+
+var initialSchool = new School(input);
+
+var dayEightyCount = Operations.RunSimulation(initialSchool, 80).SchoolSize;
+
+Console.WriteLine($"Part 1 - After 80 days, there are {dayEightyCount} fish.");

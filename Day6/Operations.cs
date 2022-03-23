c7505390eb0ddad58da580ce7f10428783c48fdb
Cail Young <cail.young@octopus.com>
2022-03-23 21:06:44 +1100

    pass the test

--- Day6/Operations.cs
+++ Day6/Operations.cs
@@ -7,9 +7,15 @@ public class Operations
     public static School AddADay(School currentSchool)
     {
         var workingSchoolFish = currentSchool.CurrentFish
-            .Select(f => new LanternFish(f.DaysUntilBirth - 1))
-            .ToImmutableList();
+            .Select(f => new LanternFish(f.DaysUntilBirth - 1)).ToList();
+
+        var countOfFishToBirth = workingSchoolFish.Count(f => f.DaysUntilBirth < 0);
+
+        workingSchoolFish.RemoveAll(f => f.DaysUntilBirth < 0);
         
-        return new School(workingSchoolFish);
+        workingSchoolFish.AddRange(Enumerable.Repeat(new LanternFish(), countOfFishToBirth));
+        workingSchoolFish.AddRange(Enumerable.Repeat(new LanternFish(6), countOfFishToBirth));
+        
+        return new School(workingSchoolFish.ToImmutableList());
     }
 }

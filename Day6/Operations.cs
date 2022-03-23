33689cef6b2d6e5de22a458ee80a68db498a33cf
Cail Young <cail.young@octopus.com>
2022-03-23 22:15:03 +1100

    I've got a bad feeling about this...

--- Day6/Operations.cs
+++ Day6/Operations.cs
@@ -1,4 +1,5 @@
 using System.Collections.Immutable;
+using System.Diagnostics;
 
 namespace Day6;
 
@@ -7,25 +8,31 @@ public class Operations
     public static School AddADay(School currentSchool)
     {
         var workingSchoolFish = currentSchool.CurrentFish
-            .Select(f => new LanternFish(f.DaysUntilBirth - 1)).ToList();
+            .Select(f => new LanternFish(f.DaysUntilBirth - 1)).ToArray();
 
         var countOfFishToBirth = workingSchoolFish.Count(f => f.DaysUntilBirth < 0);
 
-        workingSchoolFish.RemoveAll(f => f.DaysUntilBirth < 0);
-        
-        workingSchoolFish.AddRange(Enumerable.Repeat(new LanternFish(), countOfFishToBirth));
-        workingSchoolFish.AddRange(Enumerable.Repeat(new LanternFish(6), countOfFishToBirth));
-        
-        return new School(workingSchoolFish.ToImmutableList());
+        var newSchoolFish = workingSchoolFish
+            .Where(f => f.DaysUntilBirth >= 0)
+            .Concat(Enumerable.Repeat(new LanternFish(), countOfFishToBirth))
+            .Concat(Enumerable.Repeat(new LanternFish(6), countOfFishToBirth));
+
+        return new School(newSchoolFish.ToImmutableList());
     }
 
     public static School RunSimulation(School initialSchool, int daysToSimulate)
     {
         var workingSchool = new School(initialSchool.CurrentFish);
+
+        var stopwatch = new Stopwatch();
         
         for (int i = 0; i < daysToSimulate; i++)
         {
+            stopwatch.Start();
             workingSchool = AddADay(workingSchool);
+            Console.WriteLine($"Iteration {i}, elapsed time {stopwatch.Elapsed.TotalMilliseconds}");
+            stopwatch.Stop();
+            stopwatch.Reset();
         }
 
         return workingSchool;

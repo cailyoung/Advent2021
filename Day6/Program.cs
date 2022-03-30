69d03aafe8e6fc9290dc7e92b1bd3027e9bad18e
Cail Young <cail.young@octopus.com>
2022-03-30 14:56:03 +1100

    signoff message

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -17,4 +17,4 @@ Console.WriteLine($"Part 1 - After 80 days, there are {dayEightyCount} fish.");
 
 var day256Count = Operations.RunSimulation(initialSchool, 256).SchoolSize;
 
-Console.WriteLine($"Part 2 - After 256 days, there are {day256Count} fish.");
+Console.WriteLine($"Part 2 - After 256 days, there are {day256Count} fish. That's a lot of fish!");

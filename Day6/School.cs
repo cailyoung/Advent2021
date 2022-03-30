8a18e5404b39562fab226cc3b4c1afbd97970b3c
Cail Young <cail.young@octopus.com>
2022-03-30 13:21:32 +1100

    scaffold out dict for school representation

--- Day6/School.cs
+++ Day6/School.cs
@@ -5,15 +5,24 @@ namespace Day6;
 public class School
 {
     public ImmutableList<LanternFish> CurrentFish { get; }
+    public ImmutableDictionary<int, int> CurrentFishDict { get;  }
     public long SchoolSize => CurrentFish.Count;
 
     public School(ImmutableList<LanternFish> currentFish)
     {
         CurrentFish = currentFish;
+        CurrentFishDict = currentFish
+            .GroupBy(fish => fish.DaysUntilBirth)
+            .Select(fishes => new { Age = fishes.Key, Count = fishes.Count() })
+            .ToImmutableDictionary(group => group.Age, group => group.Count);
     }
 
     public School(IEnumerable<int> fishSizes)
     {
         CurrentFish = fishSizes.Select(s => new LanternFish(s)).ToImmutableList();
+        CurrentFishDict = fishSizes.GroupBy(v => v)
+            .Select(fishes => new { Age = fishes.Key, Count = fishes.Count() })
+            .ToImmutableDictionary(group => group.Age, group => group.Count);
+        ;
     }
 }

4c9bd391b5ec71894ebe3af7b1174fdda2ee32e1
Cail Young <cail.young@octopus.com>
2022-03-30 14:48:51 +1100

    part 2 solution

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -12,3 +12,9 @@ var initialSchool = new School(input);
 var dayEightyCount = Operations.RunSimulation(initialSchool, 80).SchoolSize;
 
 Console.WriteLine($"Part 1 - After 80 days, there are {dayEightyCount} fish.");
+
+// Part 2
+
+var day256Count = Operations.RunSimulation(initialSchool, 256).SchoolSize;
+
+Console.WriteLine($"Part 2 - After 256 days, there are {day256Count} fish.");

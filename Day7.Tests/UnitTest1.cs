dc23c6dc603c02c74bba05c710a4edceab4c6251
Cail Young <cail.young@octopus.com>
2022-04-01 20:21:30 +1100

    scaffold and initial failing test

--- Day7.Tests/UnitTest1.cs
+++ Day7.Tests/UnitTest1.cs
@@ -1,11 +1,31 @@
+using System;
+using System.Linq;
+using FluentAssertions;
 using Xunit;
 
 namespace Day7.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData(1, 41)]
+    [InlineData(3, 39)]
+    [InlineData(10, 71)]
+    [InlineData(2, 37)]
+    public void TargetPositionGetsCorrectFuelUsage(int targetPosition, int expectedFuelConsumed)
     {
+        var inputData = "16,1,2,0,4,2,7,1,2,14"
+            .Split(",")
+            .Select(v => Convert.ToInt32(v));
+
+        var inputSubField = new SubmarineField(inputData);
+        
+        var actualUsage = inputSubField
+            .GetFuelConsumptionValues()
+            .Where(v => v.TargetPosition == targetPosition)
+            .Select(v => v.FuelConsumed)
+            .Single();
+
+        actualUsage.Should().Be(expectedFuelConsumed);
     }
 }
--- Day7/Submarines.cs
+++ Day7/Submarines.cs
@@ -13,4 +13,21 @@ public class SubmarineField
     }
 
     public IEnumerable<Crab> Crabs { get; }
+
+    public IEnumerable<FuelConsumptionValue> GetFuelConsumptionValues()
+    {
+        return new[] { new FuelConsumptionValue(default, default) };
+    }
+}
+
+public class FuelConsumptionValue
+{
+    public FuelConsumptionValue(int targetPosition, int fuelConsumed)
+    {
+        TargetPosition = targetPosition;
+        FuelConsumed = fuelConsumed;
+    }
+
+    public int TargetPosition { get; }
+    public int FuelConsumed { get; }
 }

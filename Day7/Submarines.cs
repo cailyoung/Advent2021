84913c30d09f6247d6879736642f654416d5db7d
Cail Young <cail.young@octopus.com>
2022-04-01 20:35:09 +1100

    unused

--- Day7/Submarines.cs
+++ Day7/Submarines.cs
@@ -2,11 +2,6 @@ namespace Day7;
 
 public class SubmarineField
 {
-    public SubmarineField(IEnumerable<Crab> crabs)
-    {
-        Crabs = crabs;
-    }
-
     public SubmarineField(IEnumerable<int> horizontalPositions)
     {
         Crabs = horizontalPositions.Select(p => new Crab(p));

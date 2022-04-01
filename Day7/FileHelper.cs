9b2b0f54e11cfead50f975f076cb9afbe99d070a
Cail Young <cail.young@octopus.com>
2022-04-01 19:59:11 +1100

    add parser

---
+++ Day7/FileHelper.cs
@@ -0,0 +1,11 @@
+namespace Day7;
+
+public class FileHelper
+{
+    public static IEnumerable<int> ExtractInputFromFile(string inputFileName)
+    {
+        var rawInput = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, inputFileName));
+
+        return rawInput.SelectMany(s => s.Split(",")).Select(n => Convert.ToInt32(n));
+    }
+}

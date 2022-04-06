71d0e8c8c6fb9d2d516c53a2fac9494517a25b89
Cail Young <cail.young@octopus.com>
2022-04-06 20:38:32 +1000

    scaffold

--- Day7/FileHelper.cs
+++ Day7/FileHelper.cs
@@ -8,4 +8,9 @@ public class FileHelper
 
         return rawInput.SelectMany(s => s.Split(",")).Select(n => Convert.ToInt32(n));
     }
+
+    public static string[] SplitInputLine(string inputLine)
+    {
+        return new string[2];
+    }
 }

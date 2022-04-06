2ea3a45411838c9c6c0f94a0c718d8e71a110d87
Cail Young <cail.young@octopus.com>
2022-04-06 21:29:15 +1000

    part 1 solution

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Part 1
+
+using Day8;
+
+var rawInput = FileHelper.ExtractInputFromFile("day8input.txt");
+
+var outputsOnly = rawInput
+    .Select(FileHelper.SplitInputLine)
+    .Select(r => r.OutputValues)
+    .SelectMany(FileHelper.SplitDataRowSection);
+
+var partOneCount = DigitAnalyser.CalculateNumberOfUniqueSegmentCountDigits(outputsOnly);
+
+Console.WriteLine($"Part 1 - the number of times 1/4/7/8 appear in the outputs is {partOneCount}");

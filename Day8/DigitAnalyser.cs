025cf5630dce8fcf22e42e49cc23f61a8a3d6d58
Cail Young <cail.young@octopus.com>
2022-04-08 22:14:18 +1000

    extract method and scaffold decoder

--- Day8/DigitAnalyser.cs
+++ Day8/DigitAnalyser.cs
@@ -9,51 +9,69 @@ public static class DigitAnalyser
 
     public static IDictionary<Digit.Character, IEnumerable<char>> GenerateCharacterMappings(IEnumerable<Digit> inputDigits)
     {
-        /*
-         * SEGMENT MAPPING:
-         *  aaaa 
-         * b    c
-         * b    c
-         *  dddd 
-         * e    f
-         * e    f
-         *  gggg 
-         *            
-         *  
-         * '7' == 3 segments (a - c - - f -)
-         * '4' == 4 segments (- b c d - f -)
-         * '1' == 2 segments (- - c - - f -)
-         * '8' == 7 segments (a b c d e f g)
-         * Totals            (2 2 4 2 1 4 1)
-         *
-         * Other digits will be:
-         * '2' == 5 segments (a - c d e - g)
-         * '3' == 5 segments (a - c d - f g)
-         * '5' == 5 segments (a b - d - f g)
-         *                   (3 1 2 3 1 2 3)
-         *
-         * '0' == 6 segments (a b c - e f g)
-         * '6' == 6 segments (a b - d e f g)
-         * '9' == 6 segments (a b c d - f g)
-         *                   (3 3 2 2 2 3 3)
-         *
-         * Total counts in just the 'unknowns' are:
-         *                   (6 4 4 5 3 5 6)
-         * 
-         * Total counts of each segment overall are:
-         *                   (8 6 8 7 4 9 7)
-         *
-         * Segment A: (2,6)
-         * Segment B: (2,4)
-         * Segment C: (4,4)
-         * Segment D: (2,5)
-         * Segment E: (1,3)
-         * Segment F: (4,5)
-         * Segment G: (1,6)
-         *
-         * Count of chars across 'all uniques' and 'all unknowns' can identify the target segment :woohoo:
-         */
 
+        var digitArray = inputDigits.ToArray();
+        
+        var decodeMapper = GenerateDecodeMapper(digitArray);
+
+        var decodedDigits = digitArray
+            .Select(d => new Digit(DecodeSegmentString(d.OriginalString, decodeMapper)));
+        
+        return new Dictionary<Digit.Character, IEnumerable<char>>();
+    }
+
+    private static string DecodeSegmentString(string encodedSegmentString, IEnumerable<CharMapper> mapper)
+    {
+        return string.Empty;
+    }
+    
+    private static IEnumerable<CharMapper> GenerateDecodeMapper(IEnumerable<Digit> inputDigits)
+    {
+        /*
+        * SEGMENT MAPPING:
+        *  aaaa 
+        * b    c
+        * b    c
+        *  dddd 
+        * e    f
+        * e    f
+        *  gggg 
+        *            
+        *  
+        * '7' == 3 segments (a - c - - f -)
+        * '4' == 4 segments (- b c d - f -)
+        * '1' == 2 segments (- - c - - f -)
+        * '8' == 7 segments (a b c d e f g)
+        * Totals            (2 2 4 2 1 4 1)
+        *
+        * Other digits will be:
+        * '2' == 5 segments (a - c d e - g)
+        * '3' == 5 segments (a - c d - f g)
+        * '5' == 5 segments (a b - d - f g)
+        *                   (3 1 2 3 1 2 3)
+        *
+        * '0' == 6 segments (a b c - e f g)
+        * '6' == 6 segments (a b - d e f g)
+        * '9' == 6 segments (a b c d - f g)
+        *                   (3 3 2 2 2 3 3)
+        *
+        * Total counts in just the 'unknowns' are:
+        *                   (6 4 4 5 3 5 6)
+        * 
+        * Total counts of each segment overall are:
+        *                   (8 6 8 7 4 9 7)
+        *
+        * Segment A: (2,6)
+        * Segment B: (2,4)
+        * Segment C: (4,4)
+        * Segment D: (2,5)
+        * Segment E: (1,3)
+        * Segment F: (4,5)
+        * Segment G: (1,6)
+        *
+        * Count of chars across 'all uniques' and 'all unknowns' can identify the target segment :woohoo:
+        */
+        
         var charCountDecoderLookup = new Dictionary<string, char>
         {
             { "26", 'a' },
@@ -71,14 +89,14 @@ public static class DigitAnalyser
         };
 
         var workingArray = inputDigits.ToArray();
-        
+
         var uniqueSegmentCharCounts = workingArray
             .Where(d => listOfUniqueSegmentChars.Contains(d.CurrentCharacter))
             .SelectMany(d => d.OriginalString.ToList())
             .GroupBy(c => c)
             .ToDictionary(g => g.Key, g => g.Count());
-        
-        var unknownSegmentCharCounts= workingArray
+
+        var unknownSegmentCharCounts = workingArray
             .Where(d => !listOfUniqueSegmentChars.Contains(d.CurrentCharacter))
             .SelectMany(d => d.OriginalString.ToList())
             .GroupBy(c => c)
@@ -89,11 +107,9 @@ public static class DigitAnalyser
         var encodedCharCountLookup = charArray
             .ToDictionary(c => c, c => string.Concat(uniqueSegmentCharCounts[c], unknownSegmentCharCounts[c]));
 
-        var decodeMapper = charArray
+        return charArray
             .Select(c => new CharMapper
                 (c, charCountDecoderLookup[encodedCharCountLookup[c]]));
-        
-        return new Dictionary<Digit.Character, IEnumerable<char>>();
     }
 }
 

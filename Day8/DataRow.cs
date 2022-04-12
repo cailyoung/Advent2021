48abbbd3e509a804b742b4a857f4685be289c2cc
Cail Young <cail.young@octopus.com>
2022-04-12 21:53:36 +1000

    pass the test

--- Day8/DataRow.cs
+++ Day8/DataRow.cs
@@ -26,6 +26,28 @@ public class DataRow
 
     public static int GenerateOutputSectionNumber(IEnumerable<Digit> outputSection)
     {
-        return int.MinValue;
+        var combinedString = string.Concat(outputSection
+            .Select(d => MapCharacterToDigit(d.CurrentCharacter)));
+
+        return Convert.ToInt32(combinedString);
+    }
+
+    private static char MapCharacterToDigit(Digit.Character character)
+    {
+        return character switch
+        {
+            Digit.Character.Zero => '0',
+            Digit.Character.One => '1',
+            Digit.Character.Two => '2',
+            Digit.Character.Three => '3',
+            Digit.Character.Four => '4',
+            Digit.Character.Five => '5',
+            Digit.Character.Six => '6',
+            Digit.Character.Seven => '7',
+            Digit.Character.Eight => '8',
+            Digit.Character.Nine => '9',
+            Digit.Character.Unknown => '?',
+            _ => throw new ArgumentOutOfRangeException(nameof(character))
+        };
     }
 }

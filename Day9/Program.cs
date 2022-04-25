39f3100fab7858feb699430456a06b219ca540cd
Cail Young <cail.young@octopus.com>
2022-04-25 20:50:40 +1000

    part one solved

--- Day9/Program.cs
+++ Day9/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Part 1
+
+using Day9;
+
+var input = FileHelper.ExtractInputFromFile("day9input.txt");
+
+var inputMap = FileHelper.GenerateInitialHeightMap(input);
+
+var riskScorePartOne = MapOperations.GetMapRisk(inputMap);
+
+Console.WriteLine($"Part one risk score is {riskScorePartOne}");

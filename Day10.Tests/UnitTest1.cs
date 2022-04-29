1c29f6bd63fda39586cc980658f127d08dbc2a15
Cail Young <cail.young@octopus.com>
2022-04-29 16:31:22 +1000

    failing test

--- Day10.Tests/UnitTest1.cs
+++ Day10.Tests/UnitTest1.cs
@@ -1,11 +1,20 @@
+using FluentAssertions;
 using Xunit;
 
 namespace Day10.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("{([(<{}[<>[]}>{[]{[(<()>", "}")]
+    [InlineData("[[<[([]))<([[{}[[()]]]", ")")]
+    [InlineData("[{[{({}]{}}([{[{{{}}([]", "]")]
+    [InlineData("[<(<(<(<{}))><([]([]()", ")")]
+    [InlineData("<{([([[(<>()){}]>(<<{{", ">")]
+    public void Test1(string input, string expectedFailureToken)
     {
+        var actualOutput = Parsing.FindFirstCorruptToken(input);
+
+        actualOutput.Should().Be(expectedFailureToken);
     }
 }
---
+++ Day10/Parsing.cs
@@ -0,0 +1,9 @@
+namespace Day10;
+
+public static class Parsing
+{
+    public static string FindFirstCorruptToken(string inputLine)
+    {
+        return string.Empty;
+    }
+}

5843da33e1e27b60592b94cbd6724df5ecc714d4
Cail Young <cail.young@octopus.com>
2022-04-29 16:28:43 +1000

    add filehelper

---
+++ Day10/FileHelper.cs
@@ -0,0 +1,11 @@
+namespace Day10;
+
+public static class FileHelper
+{
+    public static string[] ExtractInputFromFile(string inputFileName)
+    {
+        var rawInput = File.ReadAllLines(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, inputFileName));
+
+        return rawInput;
+    }
+}

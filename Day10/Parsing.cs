0a30c681eac89c957c3e4e13caabac1a6fc60c01
Cail Young <cail.young@octopus.com>
2022-05-02 20:41:07 +1000

    passing test

--- Day10/Parsing.cs
+++ Day10/Parsing.cs
@@ -59,10 +59,50 @@ public static class Parsing
     {
         return ValidTokenPairs.Single(pair => pair.Closer == token).Opener;
     }
+    
+    private static char GetCloserForOpener(char token)
+    {
+        return ValidTokenPairs.Single(pair => pair.Opener == token).Closer;
+    }
 
     public static string GenerateClosingSequence(string incompleteLine)
     {
-        return string.Empty;
+        if (FindFirstCorruptToken(incompleteLine) != string.Empty)
+        {
+            throw new ArgumentException("Input line contained an incorrect closing token, please check your input");
+        }
+
+        var workingArray = incompleteLine.ToCharArray();
+        var unclosedOpeners = new Stack<char>();
+        bool? validCloser = null;
+        
+        foreach (var token in workingArray)
+        {
+            switch (ValidOpeners.Contains(token))
+            {
+                case true:
+                    unclosedOpeners.Push(token);
+                    break;
+                case false:
+                    validCloser = GetOpenerForCloser(token) == unclosedOpeners.Pop();
+                    break;
+            }
+
+            if (validCloser is null || (bool)validCloser) continue;
+            
+            break;
+        }
+
+        var closers = new Stack<char>();
+
+        while (unclosedOpeners.Any())
+        {
+            closers.Push(GetCloserForOpener(unclosedOpeners.Pop()));
+        }
+
+        var closingSequence = string.Concat(closers.Reverse());
+        
+        return closingSequence;
     }
     
     public static IEnumerable<string> FindCorruptLines(IEnumerable<string> input)

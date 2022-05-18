8a67053e5b20cffd5fe80afa9f65ad55d77bb56c
Cail Young <cail.young@octopus.com>
2022-05-18 22:24:17 +1000

    part one complete

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Part 1
+
+using Day11;
+
+var input = FileHelper.ExtractInputFromFile("day11input.txt");
+
+var initialMap = FileHelper.GenerateInitialEnergyMap(input);
+
+var futureState = MapOperations.ProduceFutureStepState(initialMap, 100);
+
+var flashCountInFuturePartOne = futureState.Sum(p => p.FlashCount);
+
+Console.WriteLine($"Part 1 - after 100 steps, {flashCountInFuturePartOne} flashes have occurred");

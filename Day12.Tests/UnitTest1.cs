c23a5a3c1100797652bdc78da26ecf2e8297a8b9
Cail Young <cail.young@octopus.com>
2022-05-30 22:20:13 +1000

    add additional tests

--- Day12.Tests/UnitTest1.cs
+++ Day12.Tests/UnitTest1.cs
@@ -21,4 +21,55 @@ b-end".Split(Environment.NewLine);
 
         actualPaths.Should().Be(10);
     }
+
+    [Fact]
+    public void MediumExampleHasNineteenPaths()
+    {
+        var input = @"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc".Split(Environment.NewLine);
+        
+        var actualCaveSystem = FileHelper.ParseInput(input);
+
+        var actualPaths = actualCaveSystem.ValidPaths;
+
+        actualPaths.Should().Be(19);
+    }
+
+
+    [Fact]
+    public void LargeExampleHas226Paths()
+    {
+        var input = @"fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW".Split(Environment.NewLine);
+        
+        var actualCaveSystem = FileHelper.ParseInput(input);
+
+        var actualPaths = actualCaveSystem.ValidPaths;
+
+        actualPaths.Should().Be(226);
+    }
 }

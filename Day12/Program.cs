49b5f3b49707f0f4a05444bb0e8e1e7df4e8b43c
Cail Young <cail.young@octopus.com>
2022-05-30 22:22:58 +1000

    part one done

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+// Part one
+
+using Day12;
+
+var input = FileHelper.ExtractInputFromFile("day12input.txt");
+
+var caveSystem = FileHelper.ParseInput(input);
+
+var pathCount = caveSystem.ValidPaths;
+
+Console.WriteLine($"Part 1 = the cave has {pathCount} paths through it that visit a small cave only once");

37dd6099756b74e3399f32728587eec2ad1c69db
Cail Young <cail.young@octopus.com>
2022-06-03 13:32:09 +1000

    part two solution

--- Day12/Program.cs
+++ Day12/Program.cs
@@ -9,5 +9,8 @@ var input = FileHelper.ExtractInputFromFile("day12input.txt");
 var caveSystem = FileHelper.ParseInput(input);
 
 var pathCount = caveSystem.ValidPartOnePaths;
+var pathCountTwo = caveSystem.ValidPartTwoPaths;
 
 Console.WriteLine($"Part 1 = the cave has {pathCount} paths through it that visit a small cave only once");
+
+Console.WriteLine($"Part 1 = the cave has {pathCountTwo} paths through it that visit a small cave only once");

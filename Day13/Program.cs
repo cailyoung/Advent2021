56ecc9eb26ef7b0008722e504c62d1f965526e40
Cail Young <cail.young@octopus.com>
2022-06-17 20:09:07 +1000

    solve part two

--- Day13/Program.cs
+++ Day13/Program.cs
@@ -8,10 +8,15 @@ var input = FileHelper.ExtractInputFromFile("day13input.txt");
 
 var inputPaper = new TransparentPaper(FileHelper.GetDots(input));
 
-var inputFolds = FileHelper.GetFolds(FileHelper.ExtractInputFromFile("day13folds.txt"));
+var inputFolds = FileHelper.GetFolds(FileHelper.ExtractInputFromFile("day13folds.txt")).ToArray();
 
 var firstFold = inputFolds.First();
 
 var firstFoldedPaper = inputPaper.Fold(firstFold);
 
 Console.WriteLine($"Part one - after the first fold, there are {firstFoldedPaper.DotCount} dots");
+
+var finalFoldsPaper = inputPaper.FoldMultiple(inputFolds);
+
+Console.WriteLine("Part two - after all folds, the output looks like:");
+Console.Write(finalFoldsPaper.ToString());

edc2e84bd7f0067edcdc42a156d75ba55311a90a
Cail Young <cail.young@octopus.com>
2022-06-20 22:32:40 +1000

    part one

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Day14;
+
+var input = FileHelper.ExtractInputFromFile("day14input.txt");
+
+var inputTemplate = FileHelper.GetTemplate(input);
+var inputRules = FileHelper.GetInsertionRules(input).ToArray();
+
+var workingChain = new PolymerChain(inputTemplate);
+const int roundsToCalculate = 10;
+
+for (int i = 0; i < roundsToCalculate; i++)
+{
+    workingChain = workingChain.ApplyInsertionRules(inputRules);
+}
+
+Console.WriteLine($"Part one - the difference between most common and least common elements is {workingChain.MostCommonElementCount - workingChain.LeastCommonElementCount}");
